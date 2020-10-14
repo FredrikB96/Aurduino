bfb0ff55ec80b34610fafeb1e91aaefdf658cbf9
Fredrik Blomgren <blomgren_fredrik@hotmail.com>
2020-10-14 14:21:57 +0200

    2020-10-14b

--- Test_Project/src/main.c
+++ Test_Project/src/main.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include <driver/gpio.h>
+
+
+#define PIN_LED 13
 
 void app_main()
 
 {
 
-    printf("Hello world!\n");
+    gpio_reset_pin(PIN_LED);
+    gpio_set_direction(PIN_LED, GPIO_MODE_DEF_OUTPUT);
+    while(1)
+    {
 
+    gpio_set_level(PIN_LED,1);
+    printf("Hello world!\n");
+    vTaskDelay(50 / portTICK_PERIOD_MS);
+    gpio_set_level(PIN_LED,0);    
+    
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
     
 
 }
